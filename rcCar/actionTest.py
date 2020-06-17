ac561826550da8671401d1b83ff743e1e60f06c7
peterhong27 <peterhong27@daum.net>
2020-06-17 06:07:33 +0100

    change setQuery

--- rcCar/actionTest.py
+++ rcCar/actionTest.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5 import QtSql
 from Car import Car 
+from sense_hat import SenseHat
 import time
 
 class pollingThread(QThread):
@@ -21,74 +22,104 @@ class pollingThread(QThread):
         self.start=0
         self.left_pressed = False
         self.right_pressed = False
-        self.getQuery()
-
-    def getQuery(self):
+        
+        self.sense = SenseHat()
         while True:
             time.sleep(0.1)
-            query = QtSql.QSqlQuery("select * from command1 order by time desc limit 1")
-            query.next()
-            cmdTime = query.record().value(0)
-            cmdType = query.record().value(1)
-            cmdArg = query.record().value(2)
-            is_finish = query.record().value(3)
-            
-            # 명령어 처리가 안된 경우 실행
-            if is_finish == 0:
-                query = QtSql.QSqlQuery("update command1 set is_finish=1 where is_finish=0")
-                print(cmdTime, cmdType, cmdArg)
+            self.getQuery()
+            self.setQuery()
+    def setQuery(self):
+        pressure = self.sense.get_pressure()
+        temp = self.sense.get_temperature()
+        humidity = self.sense.get_humidity()
+    
+        p = round((pressure - 1000) / 100, 3)
+        t = round(temp / 100, 3)
+        h = round(humidity / 100, 3)
+    
+        #msg = "Press : " + str(p) + "  Temp : " + str(t) + "  Humid : " + str(h)
+        #print(msg)    
+        self.query = QtSql.QSqlQuery();
+        self.query.prepare("insert into sensing (time, num1, num2, num3, meta_string, is_finish) values (:time, :num1, :num2, :num3, :meta, :finish)");
+        time = QDateTime().currentDateTime()
+        self.query.bindValue(":time", time)
+        self.query.bindValue(":num1", p)
+        self.query.bindValue(":num2", t)
+        self.query.bindValue(":num3", h)
+        self.query.bindValue(":meta", "")
+        self.query.bindValue(":finish", 0)
+        self.query.exec()
+        
+        a = int((p * 1271) % 256)
+        b = int((t * 1271) % 256)
+        c = int((h * 1271) % 256)
+        self.sense.clear(a, b, c)
 
-                if cmdType == "go":
-                    self.car.go()
-                    input_time = self.getTime(cmdArg)
-                    self.start = time.time()
-                    while True:
-                        if time.time() - self.start > input_time:
-                            self.car.stop()
-                            break
-                    
-                if cmdType == "back":
-                    self.car.back()
-                    input_time = self.getTime(cmdArg)
-                    self.start = time.time()
-                    while True:
-                        if time.time() - self.start > input_time:
-                            self.car.stop()
-                            break
+    def getQuery(self):
+        
+        query = QtSql.QSqlQuery("select * from command1 order by time desc limit 1")
+        query.next()
+        cmdTime = query.record().value(0)
+        cmdType = query.record().value(1)
+        cmdArg = query.record().value(2)
+        is_finish = query.record().value(3)
+        
+        
+        if is_finish == 0:
+            query = QtSql.QSqlQuery("update command1 set is_finish=1 where is_finish=0")
+            print(cmdTime, cmdType, cmdArg)
 
-                if cmdType == "move":
-                    self.car.move()
+            if cmdType == "go":
+                self.car.go()
+                input_time = self.getTime(cmdArg)
+                self.start = time.time()
+                while True:
+                    if time.time() - self.start > input_time:
+                        self.car.stop()
+                        break
                 
-                # left button
-                if cmdType == "left" and cmdArg == "pressed":
-                    self.car.steer_left()
-                    self.left_pressed = True
-                if cmdType == "left" and cmdArg == "released":
-                    self.left_pressed = False
+            if cmdType == "back":
+                self.car.back()
+                input_time = self.getTime(cmdArg)
+                self.start = time.time()
+                while True:
+                    if time.time() - self.start > input_time:
+                        self.car.stop()
+                        break
 
-                # right button
-                if cmdType == "right" and cmdArg == "pressed":
-                    self.car.steer_right()
-                    self.right_pressed = True
-                if cmdType == "right" and cmdArg == "released":
-                    self.right_pressed = False
-
-                if cmdType == "mid":
-                    self.car.steer_center()
-
-                if cmdType == "speedUp":
-                    self.car.speedUp()
-
-                if cmdType == "speedDown":
-                    self.car.speedDown()
+            if cmdType == "move":
+                self.car.move()
             
-            if self.left_pressed == True:
+            # left button
+            if cmdType == "left" and cmdArg == "pressed":
                 self.car.steer_left()
-            if self.right_pressed == True:
+                self.left_pressed = True
+            if cmdType == "left" and cmdArg == "released":
+                self.left_pressed = False
+
+            # right button
+            if cmdType == "right" and cmdArg == "pressed":
                 self.car.steer_right()
+                self.right_pressed = True
+            if cmdType == "right" and cmdArg == "released":
+                self.right_pressed = False
+
+            if cmdType == "mid":
+                self.car.steer_center()
+
+            if cmdType == "speedUp":
+                self.car.speedUp()
+
+            if cmdType == "speedDown":
+                self.car.speedDown()
+        
+        if self.left_pressed == True:
+            self.car.steer_left()
+        if self.right_pressed == True:
+            self.car.steer_right()
 
     def getTime(self, cmdArg):
-         # 작동 시간
+         
         return int(cmdArg.split(' ')[0])
     
     def getAngle(self, cmdArg):
